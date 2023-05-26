f85c34cc4df0bac0324eaec3e12cada7c0eb1f08
A1exsandro <alexsandro.acp@gmail.com>
2023-05-26 14:25:04 -0400

    refactor: remove AppBar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import {
-  Container,
-  AppBar,
-  Typography,
+  Container, 
   Grow,
   Grid
 } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 
 import { getPosts } from './actions/posts'
-import memories from './images/play1.png'
+
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   const [currentId, setCurrentId] = useState(null)
@@ -24,11 +23,8 @@ const App = () => {
   },[currentId, dispatch])
 
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
-      </AppBar>
+    <Container maxWidth="lg">  
+      <Navbar />
       <Grow in>
         <Container>
           <Grid container justify='space-between' alignItems='stretch' spacing={3}>
