73d07da0739d1acefe71f3c5e61bd370cbb541b6
A1exsandro <alexsandro.acp@gmail.com>
2023-05-26 13:25:37 -0400

    feat: redux actions like-post and delete-post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,3 +1,5 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes'
+
 import * as api from '../api'
 
 // ACTION CRERATORS
@@ -29,3 +31,23 @@ export const updatePost = (id, post) => async (dispatch) => {
     console.log(error.message)
   }
 }
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: LIKE, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: DELETE, payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
