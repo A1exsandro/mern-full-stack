663cdb7a3e9c003d4e0f32f74310270b96f5be4f
A1exsandro <alexsandro.acp@gmail.com>
2023-05-30 21:04:26 -0400

    feat: add signin and signup endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -7,3 +7,6 @@ export const createPost = (newPost) => axios.post(url, newPost)
 export const updatePost = (id, updatePost) => axios.patch(`${url}/${id}`, updatePost)
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`)
 export const deletePost = (id) => axios.delete(`${url}/${id}`)
+
+export const signIn = (formData) => axios.post('/user/signin', formData);
+export const signUp = (formData) => axios.post('/user/signup', formData);
