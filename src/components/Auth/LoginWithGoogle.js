e994736e5c9df1f0c16c8d57c88a738fa2eea4a5
A1exsandro <alexsandro.acp@gmail.com>
2023-06-09 12:00:13 -0400

    feat: login with firebase and google

--- src/components/Auth/LoginWithGoogle.js
+++ src/components/Auth/LoginWithGoogle.js
@@ -1,7 +1,61 @@
+import React, { useState } from 'react'
+import {  
+	GoogleAuthProvider, 
+	signInWithPopup, 
+	createUserWithEmailAndPassword 
+} from "firebase/auth"
+import { auth } from '../../services/firebase.js'
 
 const LoginWithGoogle = () => {
+  const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('') // to make them
+
+  // LOGIN FIREBASE WITH EMAIL  
+	const loginWithEmail = () => createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code
+    const errorMessage = error.message
+    // ..
+  })
+
+  // LOGIN FIREBASE WITH GOOGLE 
+	const provider = new GoogleAuthProvider() 
+	const loginWithGoogle = () => signInWithPopup(auth, provider) 
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+		// setInfoUser(user)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+		console.log(credential)
+    // ...
+  })
+
   return (
-    <div>Login With Google</div>
+    <div>
+      Login With Google
+      <button
+        onClick={loginWithGoogle}
+      >
+        Login With Google
+      </button>
+    </div>
   )
 }
 
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
+import { getDatabase, ref, set, get, push, onValue } from "firebase/database" 
+import { getStorage } from "firebase/storage"
+import { getAuth } from "firebase/auth"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,5 +21,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const database = getDatabase(app)
+const analytics = getAnalytics(app)
+
+export { database, ref, set, get, push, onValue, getStorage, auth }
