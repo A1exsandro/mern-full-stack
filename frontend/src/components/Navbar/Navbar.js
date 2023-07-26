99c583060ed7ee9c98585f3de55884c242c93f6f
A1exsandro <alexsandro.acp@gmail.com>
2023-07-26 10:41:20 -0400

    refactor: navbar component for better readability

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -17,18 +17,14 @@ const Navbar = () => {
 
   const logout = () => {
     dispatch({ type: actionType.LOGOUT })
-
     history.push('/auth')
-
     setUser(null)
   }
 
   useEffect(() => {
     const token = user?.token
-
     if (token) {
       const decodedToken = decode(token)
-
       if (decodedToken.exp * 1000 < new Date().getTime()) logout()
     }
 
@@ -38,18 +34,52 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography ////////////////////////// TITLE WITH LOGO
+          component={Link} 
+          to="/" 
+          className={classes.heading} 
+          variant="h2" 
+          align="center"
+        >
+          Memories
+        </Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
+
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
-          <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+          <div className={classes.profile}> 
+            <Avatar ///////////////////////////// AVATAR OF USER
+              className={classes.purple} 
+              alt={user?.result.name} 
+              src={user?.result.imageUrl}
+            >
+              {user?.result.name.charAt(0)}
+            </Avatar>
+            <Typography ////////////////////////// USER NAME
+              className={classes.userName} 
+              variant="h6"
+            >
+              {user?.result.name}
+            </Typography>
+            <Button /////////////////////////////// LOGO OUT
+              variant="contained" 
+              className={classes.logout} 
+              color="secondary" 
+              onClick={logout}
+            >
+              Logout
+            </Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button ////////////////////////////////// SIGN IN
+            component={Link} 
+            to="/auth" 
+            variant="contained" 
+            color="primary"
+          >
+            Sign In
+          </Button>
         )}
       </Toolbar>
     </AppBar>
